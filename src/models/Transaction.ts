5eb86d47e7edb7630915316eaff312eb636ddb26
jpbadan <jpbadan@gmail.com>
2020-10-02 11:22:52 +0200

    Database Migrations OK

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,35 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  JoinColumn,
+  ManyToOne,
+  OneToMany,
+} from 'typeorm';
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column('integer')
   value: number;
 
-  category_id: string;
+  @ManyToOne(() => Category, category => category.id)
+  // @JoinColumn({ name: 'category_id' })
+  category_id: Category;
 
+  @Column('timestamp')
   created_at: Date;
 
+  @Column('timestamp')
   updated_at: Date;
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,42 @@
+import { getRepository } from 'typeorm';
+import { uuid } from 'uuidv4';
+import Category from '../models/Category';
 // import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
 
+interface TransactionRequest {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: TransactionRequest): Promise<Transaction> {
+    const categoriesRepository = getRepository(Category);
+
+    const categorie = categoriesRepository.create({
+      title: category,
+    });
+    await categoriesRepository.save(categorie);
+
+    const transactionsRepository = getRepository(Transaction);
+
+    const transaction = transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+
+    await transactionsRepository.save(transaction);
+
+    return transaction;
   }
 }
 
